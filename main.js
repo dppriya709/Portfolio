2f49e6613fe75e5b88553181e7a160cb9d0dccaa
Priya D P <166364424+dppriya709@users.noreply.github.com>
2024-06-15 16:21:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed=new Typed('#typed',{
+    strings: ["web development","web design","Frontend developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+
+});
